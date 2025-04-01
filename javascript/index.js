a8ac3a9257e5dc633722c5121b7b3da363069705
jhozera <152558072+jhozera@users.noreply.github.com>
2025-04-01 11:30:26 -0300

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -63,3 +63,21 @@ const observer = new IntersectionObserver((entries) => {
 document.querySelectorAll(".deal-box h4").forEach((num) => {
     observer.observe(num);
 });
+
+
+
+
+// faq
+function showContente(id) {
+    const content = document.getElementById(id);
+    const isVisible = content.style.display === "block";
+  
+    const allContents = document.querySelectorAll('.bt-content');
+    allContents.forEach(el => el.style.display = 'none');
+  
+    if (!isVisible) {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+  }
